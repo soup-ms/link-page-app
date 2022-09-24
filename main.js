b437a4988b2e0a3af225b26bedc553ca3aac8b37
Michael <michaelsousajr@gmail.com>
2022-09-24 01:21:12 -0400

    commit: 1.0

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () =>{
+        
+    VANTA.NET({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        points: 20.00,
+        maxDistance: 26.00
+      })
+
+    setTimeout(() => {
+    const main = document.querySelector('main')
+    main.style.opacity = 1
+    main.style.filter = 'blur(0px)'
+    }, 10) 
+        
+})
+
+
+function discordMsg(){
+    alert("Soup#4235")
+}
+
+function websiteMsg(){
+    alert("Not Yet Available")
+}
+
+function snapchatMsg(){
+    alert("michaelsousa.00")
+}
