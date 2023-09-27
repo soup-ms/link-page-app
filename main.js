bbb2ef071e9a6783f5a89bdafc81d31437c505ce
Michael <89663481+michaelsousajr@users.noreply.github.com>
2023-09-26 22:07:32 -0400

    bg changes and fixed links

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 window.addEventListener('DOMContentLoaded', () =>{
-        
-    VANTA.NET({
-        el: "#vanta",
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: true,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        points: 20.00,
-        maxDistance: 26.00
-      })
-
-    setTimeout(() => {
+   setTimeout(() => {
     const main = document.querySelector('main')
     main.style.opacity = 1
     main.style.filter = 'blur(0px)'
     }, 10) 
-        
 })
 
 
 function discordMsg(){
-    alert("Soup#4235")
+    alert("soupz_")
 }
 
 function websiteMsg(){
@@ -31,8 +16,5 @@ function websiteMsg(){
 }
 
 function snapchatMsg(){
-    alert("michaelsousa.00")
+    alert("michaelsousajr")
 }
-
-var rand = Math.floor(Math.random() * 360);
-document.getElementById('vanta').style.filter = `hue-rotate(${rand}deg)`;
