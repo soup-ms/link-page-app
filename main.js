7e1cff8d839984245281f7479447dd79ce444286
Michael <89663481+michaelsousajr@users.noreply.github.com>
2024-07-01 09:22:41 -0500

    new bg, added new links

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-window.addEventListener('DOMContentLoaded', () =>{
-   setTimeout(() => {
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
     const main = document.querySelector('main')
     main.style.opacity = 1
     main.style.filter = 'blur(0px)'
-    }, 10) 
+  }, 100)
 })
 
 
-function discordMsg(){
-    alert("soupz_")
+function discordMsg() {
+  alert("soupz_")
 }
 
-function snapchatMsg(){
-    alert("michaelsousajr")
+function snapchatMsg() {
+  alert("michaelsousajr")
 }
+
